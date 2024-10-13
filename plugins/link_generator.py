7a5cbf582b45889d740357fec57bb6d4b0a03357
Boopathi9795 <boopathim693@gmail.com>
2024-10-13 18:36:57 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -38,7 +38,8 @@ async def batch(client: Client, message: Message):
         
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
+    #link = f"https://t.me/{client.username}?start={base64_string}"
+    link = f"https://kuttyfile.storebot.workers.dev?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await second_message.reply_text(f"<b>🧑‍💻 Here is your code : \n<code>{base64_string}</code></b>\n\n<b>🔗 Here is your link :</b>\n{link}", quote=True, reply_markup=reply_markup)
 
